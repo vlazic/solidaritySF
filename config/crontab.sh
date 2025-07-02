6055b5d8ff385f4f2be6018449ca02340dddae15
Darko Todorić <7748474+darkotodoric@users.noreply.github.com>
2025-07-02 14:50:33 +0200

    Increase transaction create limit (#430)

--- config/crontab.sh
+++ config/crontab.sh
@@ -1,7 +1,6 @@
 
 # Transactions
-*/10 8-21 * * 1-4 php /var/www/solidaritySF/bin/console app:transaction:create 15000 > /var/www/solidaritySF/var/log/crontab-transaction-create-1-`date +\%d-\%m-\%Y`.txt
-0 7 * * 1-4 php /var/www/solidaritySF/bin/console app:transaction:create 60000 --schoolIds=1783,1784,1786,1785 > /var/www/solidaritySF/var/log/crontab-transaction-create-2-`date +\%d-\%m-\%Y`.txt
+*/10 8-21 * * 1-4 php /var/www/solidaritySF/bin/console app:transaction:create 25000 > /var/www/solidaritySF/var/log/crontab-transaction-create-1-`date +\%d-\%m-\%Y`.txt
 0 * * * * php /var/www/solidaritySF/bin/console app:transaction:expired >> /var/www/solidaritySF/var/log/crontab-transaction-expired-`date +\%d-\%m-\%Y`.txt
 10 7 * * * php /var/www/solidaritySF/bin/console app:transaction:notify-delegates >> /var/www/solidaritySF/var/log/crontab-transaction-notify-delegates-`date +\%d-\%m-\%Y`.txt
 10 7 * * * php /var/www/solidaritySF/bin/console app:transaction:notify-donors >> /var/www/solidaritySF/var/log/crontab-transaction-notify-donors-`date +\%d-\%m-\%Y`.txt
