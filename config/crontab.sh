4bec48a86b62b24d862955f9bd1543fb2934f472
Darko Todorić <7748474+darkotodoric@users.noreply.github.com>
2025-07-22 15:54:27 +0200

    New repository name (#451)

--- config/crontab.sh
+++ config/crontab.sh
@@ -1,19 +1,19 @@
 
 # Transactions
-0 7 * * 1-4 php /var/www/solidaritySF/bin/console app:transaction:create > /var/www/solidaritySF/var/log/crontab-transaction-create-`date +\%d-\%m-\%Y`.txt
-0 * * * * php /var/www/solidaritySF/bin/console app:transaction:expired >> /var/www/solidaritySF/var/log/crontab-transaction-expired-`date +\%d-\%m-\%Y`.txt
-10 7 * * * php /var/www/solidaritySF/bin/console app:transaction:notify-delegates >> /var/www/solidaritySF/var/log/crontab-transaction-notify-delegates-`date +\%d-\%m-\%Y`.txt
-10 7 * * * php /var/www/solidaritySF/bin/console app:transaction:notify-donors >> /var/www/solidaritySF/var/log/crontab-transaction-notify-donors-`date +\%d-\%m-\%Y`.txt
+0 7 * * 1-4 php /var/www/solidarityMSP/bin/console app:transaction:create > /var/www/solidarityMSP/var/log/crontab-transaction-create-`date +\%d-\%m-\%Y`.txt
+0 * * * * php /var/www/solidarityMSP/bin/console app:transaction:expired >> /var/www/solidarityMSP/var/log/crontab-transaction-expired-`date +\%d-\%m-\%Y`.txt
+10 7 * * * php /var/www/solidarityMSP/bin/console app:transaction:notify-delegates >> /var/www/solidarityMSP/var/log/crontab-transaction-notify-delegates-`date +\%d-\%m-\%Y`.txt
+10 7 * * * php /var/www/solidarityMSP/bin/console app:transaction:notify-donors >> /var/www/solidarityMSP/var/log/crontab-transaction-notify-donors-`date +\%d-\%m-\%Y`.txt
 
 # Donors
-#0 1 * * * php /var/www/solidaritySF/bin/console app:inactive-donors >> /var/www/solidaritySF/var/log/crontab-inactive-donors-`date +\%d-\%m-\%Y`.txt
-0 8 * * 1 php /var/www/solidaritySF/bin/console app:thank-you-donors >> /var/www/solidaritySF/var/log/crontab-thank-you-donors-`date +\%d-\%m-\%Y`.txt
+#0 1 * * * php /var/www/solidarityMSP/bin/console app:inactive-donors >> /var/www/solidarityMSP/var/log/crontab-inactive-donors-`date +\%d-\%m-\%Y`.txt
+0 8 * * 1 php /var/www/solidarityMSP/bin/console app:thank-you-donors >> /var/www/solidarityMSP/var/log/crontab-thank-you-donors-`date +\%d-\%m-\%Y`.txt
 
 # Cache
-*/10 * * * * php /var/www/solidaritySF/bin/console app:cache-numbers >> /var/www/solidaritySF/var/log/crontab-cache-numbers-`date +\%d-\%m-\%Y`.txt
+*/10 * * * * php /var/www/solidarityMSP/bin/console app:cache-numbers >> /var/www/solidarityMSP/var/log/crontab-cache-numbers-`date +\%d-\%m-\%Y`.txt
 
 # Cleaner
-0 2 * * * find /var/www/solidaritySF/var/log/crontab* -maxdepth 0 -type f -mtime +30 -exec rm {} \;
+0 2 * * * find /var/www/solidarityMSP/var/log/crontab* -maxdepth 0 -type f -mtime +30 -exec rm {} \;
 
 # Create damaged educator period
-10 0 * * * php /var/www/solidaritySF/bin/console app:log-numbers >> /var/www/solidaritySF/var/log/crontab-log-number-`date +\%d-\%m-\%Y`.txt
+10 0 * * * php /var/www/solidarityMSP/bin/console app:log-numbers >> /var/www/solidarityMSP/var/log/crontab-log-number-`date +\%d-\%m-\%Y`.txt
--- install.sh
+++ install.sh
@@ -12,24 +12,24 @@ else
 fi
 
 # Set git safe.directory
-docker exec solidarity-php-container git config --global --add safe.directory /var/www/project
+docker exec solidarity-msp-php-container git config --global --add safe.directory /var/www/project
 
 # Install dependencies
-docker exec solidarity-php-container php composer.phar install;
+docker exec solidarity-msp-php-container php composer.phar install;
 
 # Create database
-docker exec solidarity-php-container php bin/console doctrine:database:create --if-not-exists;
+docker exec solidarity-msp-php-container php bin/console doctrine:database:create --if-not-exists;
 
 # Dump and create tables
-docker exec solidarity-php-container php bin/console doctrine:schema:drop --force;
-docker exec solidarity-php-container php bin/console doctrine:schema:update --force;
+docker exec solidarity-msp-php-container php bin/console doctrine:schema:drop --force;
+docker exec solidarity-msp-php-container php bin/console doctrine:schema:update --force;
 
 # Load fixtures
-docker exec solidarity-php-container php bin/console doctrine:fixtures:load --group=1 --no-interaction;
-docker exec solidarity-php-container php bin/console doctrine:fixtures:load --group=2 --append --no-interaction;
-docker exec solidarity-php-container php bin/console doctrine:fixtures:load --group=3 --append --no-interaction;
-docker exec solidarity-php-container php bin/console doctrine:fixtures:load --group=4 --append --no-interaction;
-docker exec solidarity-php-container php bin/console doctrine:fixtures:load --group=5 --append --no-interaction;
-docker exec solidarity-php-container php bin/console doctrine:fixtures:load --group=6 --append --no-interaction;
+docker exec solidarity-msp-php-container php bin/console doctrine:fixtures:load --group=1 --no-interaction;
+docker exec solidarity-msp-php-container php bin/console doctrine:fixtures:load --group=2 --append --no-interaction;
+docker exec solidarity-msp-php-container php bin/console doctrine:fixtures:load --group=3 --append --no-interaction;
+docker exec solidarity-msp-php-container php bin/console doctrine:fixtures:load --group=4 --append --no-interaction;
+docker exec solidarity-msp-php-container php bin/console doctrine:fixtures:load --group=5 --append --no-interaction;
+docker exec solidarity-msp-php-container php bin/console doctrine:fixtures:load --group=6 --append --no-interaction;
 
 echo "Done!"
